d7e60d3654f0c63817c5e46e666b32f12ec25e2d
Reason239 <MrBronze@mail.ru>
2021-04-07 19:15:21 +0300

    Undebug

--- DiffAugment-stylegan2-pytorch/train.py
+++ DiffAugment-stylegan2-pytorch/train.py
@@ -538,7 +538,6 @@ def main(ctx, outdir, dry_run, **config_kwargs):
 
     # Setup training options.
     try:
-        print('Comet API ley is: ', config_kwargs['comet_api_key'])
         run_desc, args = setup_training_loop_kwargs(**config_kwargs)
     except UserError as err:
         ctx.fail(err)
