93ab0d18b4e9a54361be78c8cc85c99ce9825d6d
Ilya Blinnikov <agryosha@gmail.com>
2021-04-10 16:01:45 +0300

    Add comment

--- DiffAugment-stylegan2-pytorch/training/dataset.py
+++ DiffAugment-stylegan2-pytorch/training/dataset.py
@@ -29,7 +29,7 @@ class Dataset(torch.utils.data.Dataset):
         use_labels  = False,    # Enable conditioning labels? False = label dimension is zero.
         xflip       = False,    # Artificially double the size of the dataset via x-flips. Applied after max_size.
         random_seed = 0,        # Random seed to use when applying max_size.
-        dataset     = None
+        dataset     = None      # It is for a consistency, because kwargs with this parameter are sent to the constructor
     ):
         self._name = name
         self._raw_shape = list(raw_shape)
