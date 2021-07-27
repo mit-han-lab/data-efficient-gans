54f9dd88a474adc827bd0fdd722e3096d4b1097b
Shengyu Zhao <zsyzzsoft@gmail.com>
2021-07-28 01:41:02 +0800

    Fixed IS without validation set

--- DiffAugment-stylegan2/metrics/metric_defaults.py
+++ DiffAugment-stylegan2/metrics/metric_defaults.py
@@ -14,7 +14,7 @@ metric_defaults = EasyDict([(args.name, args) for args in [
     EasyDict(name='fid10k',       func_name='metrics.frechet_inception_distance.FID', num_images=10000, minibatch_per_gpu=8),
     EasyDict(name='fid5k-train',  func_name='metrics.frechet_inception_distance.FID', num_images=5000,  split='train', minibatch_per_gpu=8),
     EasyDict(name='fid50k-train', func_name='metrics.frechet_inception_distance.FID', num_images=50000, split='train', minibatch_per_gpu=8),
-    EasyDict(name='is10k',        func_name='metrics.inception_score.IS',             num_images=10000, num_splits=10, minibatch_per_gpu=8),
+    EasyDict(name='is10k',        func_name='metrics.inception_score.IS',             num_images=10000, split='train', num_splits=10, minibatch_per_gpu=8),
 ]])
 
 #----------------------------------------------------------------------------
